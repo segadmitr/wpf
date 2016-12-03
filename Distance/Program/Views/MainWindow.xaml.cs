9e174189427119e5f6d896ee0af0f8e1306e51a4
sega <segadmitr@yandex.ru>
2016-12-03 18:04:22 +0300

    Пустой проект, определил структуру

---
+++ Distance/Program/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DistanceCalculation.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
